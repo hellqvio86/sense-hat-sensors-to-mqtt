6c635ac188b12f58a6bca0ccd0454b2e48367e7e
Olof Hellqvist <olof.hellqvist@gmail.com>
2020-07-30 12:06:10 +0200

    Updating

--- setup.sh
+++ setup.sh
@@ -1,5 +1,10 @@
 #/bin/bash
+echo "Changing directory to $(dirname "$0")"
 cd "$(dirname "$0")"
+
+echo "Installing systemd script"
 cp systemd/sensehatsensorstomqtt.service /etc/systemd/system/sensehatsensorstomqtt.service
+
+echo "Reloading systemd"
 systemctl --user daemon-reload
 systemctl daemon-reload
