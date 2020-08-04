e6023058ef68bd2939d85473563cfd39e303d71e
Olof Hellqvist <olof.hellqvist@gmail.com>
2020-08-04 16:49:09 +0200

    Fixing

--- src/sensehatsensorstomqtt/__init__.py
+++ src/sensehatsensorstomqtt/__init__.py
@@ -19,8 +19,6 @@ from async_cron.schedule import Scheduler
 
 sys.path.append(os.path.split(os.path.dirname(sys.argv[0]))[0])
 
-print("Path: {}".format(os.path.split(os.path.dirname(sys.argv[0]))[0]))
-
 LOGGER = logging.getLogger(__name__)
 CONFIG = {}
 
