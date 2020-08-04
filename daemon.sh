7e4f09e8535486d75426facc11180a0d8723b330
Olof Hellqvist <olof.hellqvist@gmail.com>
2020-08-04 08:07:40 +0200

    FIxing

--- daemon.sh
+++ daemon.sh
@@ -1,3 +1,3 @@
 #/bin/bash
-cd /home/pi/git/sensehatsensorstomqtt
+cd /home/pi/git/sense-hat-sensors-to-mqtt
 /usr/bin/python3 -m src.sensehatsensorstomqtt.__main__ --config_file /etc/sensehatsensorstomqtt.yaml --daemon
