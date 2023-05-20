9024f7696f75bb914e506d61055306b260b56aa8
Olof Hellqvist <olof.hellqvist@gmail.com>
2023-05-20 12:04:13 +0200

    Modify is night

--- src/sensehatsensorstomqtt/utils.py
+++ src/sensehatsensorstomqtt/utils.py
@@ -8,8 +8,9 @@ def is_night(*, current_time=datetime.datetime.now()):
     """
     Function to check if its night
     """
-    if current_time.hour >= 6 and current_time.hour < 19:
-        # Its not night
-        return False
-    else:
+    if current_time.hour <= 6:
         return True
+    elif current_time.hour >= 19:
+        return True
+    else:
+        return False
